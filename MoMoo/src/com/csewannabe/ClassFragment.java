93ad9a3d15e751c830bfc61f7937bfe80aba0b2e
hpxro7 <ninjaxp@hotmail.com>
2012-12-01 16:54:56 -0800

    Added Fragment to represent the class selection page.

---
+++ MoMoo/src/com/csewannabe/ClassFragment.java
@@ -0,0 +1,7 @@
+package com.csewannabe;
+
+import android.support.v4.app.Fragment;
+
+public class ClassFragment extends Fragment {
+
+}
---
+++ MoMoo/src/com/csewannabe/DataCollector.java
@@ -0,0 +1,7 @@
+package com.csewannabe;
+
+public class DataCollector {
+	
+	public String[] classes  = {"Math", "Chemisty", "Sexology", "CSE"};
+	
+}
