0b917454236ac48b01847f7925d6266867ef27cc
hpxro7 <ninjaxp@hotmail.com>
2012-12-01 17:02:11 -0800

    God help us.

--- MoMoo/src/com/csewannabe/ClassFragment.java
+++ MoMoo/src/com/csewannabe/ClassFragment.java
@@ -1,7 +1,19 @@
 package com.csewannabe;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class ClassFragment extends Fragment {
-
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
 }
