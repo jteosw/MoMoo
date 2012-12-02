9a5966384be321479b444ddf931efaf2a50c4dac
shermpay <shermpay@uw.edu>
2012-12-01 18:58:15 -0800

    Revert "TwoFingerScrollView"
    This reverts commit 8c4b4e7bc1d74f309a6c67047f16cc4cdda16049.

--- MoMoo/gen/com/csewannabe/R.java
+++ MoMoo/gen/com/csewannabe/R.java
@@ -12,15 +12,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int list_background=0x7f020001;
     }
     public static final class id {
         public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
-        public static final int list_item_textview=0x7f030000;
-        public static final int momoo_start=0x7f030001;
-        public static final int selection_activity=0x7f030002;
+        public static final int momoo_start=0x7f030000;
+        public static final int selection_activity=0x7f030001;
     }
     public static final class menu {
         public static final int momoo_start=0x7f060000;
