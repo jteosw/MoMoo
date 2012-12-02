888f3c94445f5dc3bdf35c369b808998ed6d0efc
JTeo <jteosw@gmail.com>
2012-12-01 23:05:16 -0800

    New Canvas View with  new CONSTRUCTOR

--- MoMoo/src/com/csewannabe/MainCanvasView.java
+++ MoMoo/src/com/csewannabe/MainCanvasView.java
@@ -7,6 +7,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -28,6 +30,19 @@ public class MainCanvasView extends SurfaceView implements OnTouchListener, Runn
 	private Path path;
 	private Bitmap buffer;
 	private Canvas bufCanvas;
+	boolean doubleTouch;
+	private float prevX;
+	private float prevY;
+	private int newY;
+	private float newX;
+	private Rect blanksrc;
+	private Rect blankdest;
+	private Rect bufsrc;
+	private Rect bufdest;
+	
+	public MainCanvasView(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+	}
 	
 	public MainCanvasView(Context context, Bitmap picture) {
 		super(context);
@@ -38,13 +53,16 @@ public class MainCanvasView extends SurfaceView implements OnTouchListener, Runn
 		painter.setStyle(Paint.Style.STROKE);
 		painter.setStrokeWidth(2);
 		setOnTouchListener(this);
-		picDimensions = new int[] {picture.getHeight(), picture.getWidth()};
 		
-		src = new Rect(0,0,picDimensions[0],picDimensions[1]);			//default size of pic
-		dest = new Rect(100,100,picDimensions[0]*2+100,picDimensions[1]*2+100); //choose the location, left.top.right.btm
+		src = new Rect(0,0,picture.getWidth(),picture.getHeight());			//default size of pic
+		dest = new Rect(100,100, picture.getWidth() * 2 + 100, picture.getHeight() * 2 + 100); //choose the location, left.top.right.btm
 		blankMap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888);
+		blanksrc = new Rect(0,0,blankMap.getWidth(), blankMap.getHeight());
+		blankdest = new Rect(0,0,blankMap.getWidth(), blankMap.getHeight());
 		bmCanvas = new Canvas(blankMap);
 		buffer = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888);
+		bufsrc = new Rect(0,0,buffer.getWidth(), buffer.getHeight());
+		bufdest = new Rect(0,0,buffer.getWidth(), buffer.getHeight());
 		bufCanvas = new Canvas(buffer);
 		path = new Path();
 	}
@@ -76,36 +94,67 @@ public class MainCanvasView extends SurfaceView implements OnTouchListener, Runn
 	}
 
 	public boolean onTouch(View v, MotionEvent event) {
-		if(event.getAction() == MotionEvent.ACTION_DOWN) {
+		
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_MOVE: 
+			final int historySize = event.getHistorySize();	
+			int numPointers = event.getPointerCount();
+
+			if (numPointers < 2) {
+				for (int h = 0; h < historySize; h++) {
+					for(int p = 0; p < event.getPointerCount(); p++) {
+						path.lineTo(event.getHistoricalX(p, h),event.getHistoricalY(p,h));
+						//bmCanvas.drawCircle(event.getHistoricalX(p, h), event.getHistoricalY(p, h), 3,painter);
+						path.offset(0, newY);
+						bufCanvas.drawPath(path, painter);
+						
+						path.moveTo(event.getHistoricalX(p, h),event.getHistoricalY(p,h));
+				    }
+				}
+			} else 	if (numPointers > 1) {
+				Log.d("TOUCHED", "Am scrolling!");
+                float currX = event.getRawX();
+                float deltaX = -(currX - prevX);
+                newX += deltaX;
+                float currY = event.getRawY();
+                float deltaY = -(currY - prevY);
+                newY += deltaY;
+                dest.set(100, 100-newY, 100+picture.getWidth() * 2, 100-newY + picture.getHeight() * 2);
+                blankdest.set(0,0-newY, blankMap.getWidth(), blankMap.getWidth() - newY);
+                bufdest.set(0,0-newY, buffer.getWidth(), buffer.getWidth() - newY);
+                prevX = currX;
+                prevY = currY;
+			}
+			
+			break;
+			
+		
+		case MotionEvent.ACTION_DOWN: 
 			//bmCanvas.drawCircle(event.getX(), event.getY(), 3,painter);
 			bmCanvas.drawPath(path, painter);
-			path = new Path();
-			path.moveTo(event.getX(), event.getY());
-			
-		} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+				path = new Path();
+				path.moveTo(event.getX(), event.getY());
+			//Sets the start scroll point
+				
+				prevX = event.getRawX();
+				prevY = event.getRawY();
+				break;
+		default:
+			break;
 		
-			final int historySize = event.getHistorySize();			
-			for (int h = 0; h < historySize; h++) {
-				for(int p = 0; p < event.getPointerCount(); p++) {
-					path.lineTo(event.getHistoricalX(p, h),event.getHistoricalY(p,h));
-					//bmCanvas.drawCircle(event.getHistoricalX(p, h), event.getHistoricalY(p, h), 3,painter);
-					bufCanvas.drawPath(path, painter);
-					path.moveTo(event.getHistoricalX(p, h),event.getHistoricalY(p,h));
-				}
-			}
 		}
-		
 		return true;
 	}
-	
+		
+
 	public void run() {
 		while(isRunning) {
 			if(sHolder.getSurface().isValid()) {
 				Canvas drawingCanvas = sHolder.lockCanvas();
 				drawingCanvas.drawARGB(255, 255, 255, 255);
 				drawingCanvas.drawBitmap(picture, src,dest, null);
-				drawingCanvas.drawBitmap(blankMap, 0,0, null);
-				drawingCanvas.drawBitmap(buffer,  0, 0, null);
+				drawingCanvas.drawBitmap(blankMap, blanksrc,blankdest, null);
+				drawingCanvas.drawBitmap(buffer,  bufsrc, bufdest, null);
 				
 				
 				sHolder.unlockCanvasAndPost(drawingCanvas);
