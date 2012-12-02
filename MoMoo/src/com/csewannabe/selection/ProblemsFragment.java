0c0edc43d76a19b032eb3f33f730de138f3ed773
hpxro7 <ninjaxp@hotmail.com>
2012-12-02 09:55:52 -0800

    Nearing the final!

--- MoMoo/src/com/csewannabe/selection/ProblemsFragment.java
+++ MoMoo/src/com/csewannabe/selection/ProblemsFragment.java
@@ -1,18 +1,26 @@
 package com.csewannabe.selection;
 
-import com.csewannabe.R;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.csewannabe.R;
 
 public class ProblemsFragment extends ListFragment {
 	
+	public static final String CURR_ASSIGN = "com.csewannabe.selection.AssignmentFragment.assign";
+	public static final String PREV_DATA = "com.csewannabe.selection.AssignmentFragment.prev";
+	public static final String CURR_DATA = "com.csewannabe.selection.AssignmentFragment.curr";
+	public static final String NEXT_DATA = "com.csewannabe.selection.AssignmentFragment.next";
+	
 	String mAssignment;
 	DataCollector mData;
+	String nextProblem;
+	String prevProblem;
 	
 	public ProblemsFragment(String assignmentChosen) {
 		super();
@@ -33,6 +41,30 @@ public class ProblemsFragment extends ListFragment {
 		View finalView = inflater.inflate(R.layout.listfragment, null);
 		
 		return finalView;
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		String selected = (String) getListView().getItemAtPosition(position);
+		
+		Intent intent = new Intent(getActivity(), com.csewannabe.CanvasActivity.class);
+		
+		intent.putExtra(CURR_DATA, selected);
+		intent.putExtra(CURR_ASSIGN, mAssignment);
+		if(position + 1 <= getListView().getLastVisiblePosition()) {
+			intent.putExtra(NEXT_DATA, (String) getListView().getItemAtPosition(position+1));
+		} else {
+			intent.putExtra(NEXT_DATA, (String) null);
+		}
+		
+		if (position >= 1) {
+			intent.putExtra(PREV_DATA, (String) getListView().getItemAtPosition(position-1));
+		} else {
+			intent.putExtra(PREV_DATA, (String) null);
+		}
+		
+		startActivity(intent);
 	}
 	
 	@Override
