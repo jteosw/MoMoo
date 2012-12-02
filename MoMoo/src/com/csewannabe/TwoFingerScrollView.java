b449c525bc1dfcf5d874a7cc044bb92ed2c9a7d5
shermpay <shermpay@uw.edu>
2012-12-01 19:09:32 -0800

    commitdelte

--- MoMoo/src/com/csewannabe/TwoFingerScrollView.java
+++ MoMoo/src/com/csewannabe/TwoFingerScrollView.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
-//TEST commit
 
 public class TwoFingerScrollView extends View {
 	private Bitmap bmp;
